af5d1cd43b5f1478ee1b54c52755f06688a85f5a
juned012 <junedansari9172@gmai.com>
2025-04-09 12:49:34 +0530

    Implemented a AddStudent controller

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -4,7 +4,22 @@ let students = [
   { id: 3, name: "Alice Johnson", age: 19, city: "Chicago" },
 ];
 
-const addStudent = (req, res) => {};
+const addStudent = (req, res) => {
+  try {
+    const { name, age, city } = req.body;
+    const newStudent = { id: students.length + 1, name, age, city };
+    students.push(newStudent);
+    res.status(200).json({
+      message: "Student Added Successfully",
+      newStudent,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Student not Added",
+      error: error.message,
+    });
+  }
+};
 
 const getStudents = (req, res) => {
   res.status(200).json(students);
@@ -12,4 +27,4 @@ const getStudents = (req, res) => {
 
 const deleteStudent = (req, res) => {};
 
-module.exports = { addStudent, getStudents, updateStudent, deleteStudent };
+module.exports = { addStudent, getStudents, deleteStudent };
